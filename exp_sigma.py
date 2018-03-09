446986adbc0f53a7a3cb9e0f535e1d4814bea19c
andrea <andrea.battistello1994@gmail.com>
2018-03-09 11:55:12 +0100

    Added parameterization for sigma

--- exp_sigma.py
+++ exp_sigma.py
@@ -5,6 +5,10 @@ import math
 import itertools
 import os
 
+
+MIN_SIGMA = 0.1
+MAX_SIGMA = 30
+
 class Constants(Enum):
     ONLY_THETA = 0
     THETA_AND_SIGMA = 1
@@ -18,7 +22,63 @@ class Constants(Enum):
     def __str__(self):
         return self.name
 
-MIN_SIGMA = 0.1
+
+class SigmaFunction(object):
+    def __init__(self, param):
+        self.initial_param = param
+        self.param = param
+        self.description = 'sigma'
+
+    def update(self, step_size, gradient_sigma):
+        raise NotImplemented('Function not implemented')
+
+    def eval(self):
+        raise NotImplemented('Function not implemented')
+
+    def reset(self):
+        self.param = self.initial_param
+
+    def __str__(self):
+        return self.description
+
+
+class Identity(SigmaFunction):
+    def __init__(self, param):
+        super().__init__(param)
+        self.description = str(param)
+
+    def update(self, step_size, gradient_sigma):
+        new_param = self.param + step_size*gradient_sigma
+        new_param = max(new_param, MIN_SIGMA)
+
+        self.param = new_param
+
+    def eval(self):
+        return self.param
+
+class Exponential(SigmaFunction):
+    def __init__(self, param):
+        super().__init__(param)
+        self.description = 'e(' + str(self.param) + ')'
+
+    def update(self, step_size, gradient_sigma):
+        new_param = self.param + step_size * gradient_sigma * math.exp(self.param)
+        new_param = min(new_param, math.log(MAX_SIGMA))
+        new_param = max(new_param, math.log(MIN_SIGMA))
+
+        self.param = new_param
+
+    def eval(self):
+        return math.exp(self.param)
+
+class Lambda(SigmaFunction):
+    def __init__(self, param, coeff):
+        super().__init__(param)
+        self.coeff = coeff
+        self.description = 'lambda(' + str(self.coeff) + ')'
+
+    def update(self, step_size, gradient_sigma, ):
+        pass
 
 
 def my_range(r):
@@ -35,15 +95,17 @@ def computeLoss(R, M, gamma, volume, sigma):
 
 def run_experiment( experiment_type=Constants.ONLY_THETA,
                     theta=-0.1,
-                    sigma=20,
+                    sigma_fun=Identity(1),
                     alphaSigma=0.0001,
                     n_iterations=-1,
-                    eps=1e-03,
+                    eps=1e-01,
                     filename=None,
                     verbose=True):
-
-    initial_configuration = np.array([experiment_type.value, theta, sigma, alphaSigma, n_iterations, eps, filename])
+    initial_configuration = np.array([experiment_type.value, theta, str(sigma_fun), alphaSigma, n_iterations, eps, filename])
     lqg_env = lqg1d.LQG1D()
+
+    sigma_fun.reset()
+    sigma = sigma_fun.eval()
 
     #
     # Compute constants
@@ -62,6 +124,8 @@ def run_experiment( experiment_type=Constants.ONLY_THETA,
     m = 1
 
     for t in my_range(n_iterations):
+        sigma = sigma_fun.eval()
+
         J = lqg_env.computeJ(theta, sigma)
 
         gradK = lqg_env.grad_K(theta, sigma)
@@ -121,40 +185,51 @@ def run_experiment( experiment_type=Constants.ONLY_THETA,
         # Display
         if verbose:
             if t % 100 == 0:
-                print("\nT\t\tTheta\t\tSigma\t\tJ\t\t\tgradK\t\tgradSigma\t\tgradMixed\t\talpha\t\tupdateGradSigma\n")
-            print("{}\t\t{:.5f}\t\t{:.4f}\t\t{:.3E}\t\t{:.3f}\t\t{:.4f}\t\t{:.4f}\t\t{:.4E}\t\t{:.4f}".format(t, theta, sigma, J, gradK, gradSigma, gradMixed, alpha, updateGradSigma))
+                print("\nT\t\tTheta\t\tSigma\t\tJ\t\t\tgradK\t\tgradSigma\t\tgradMixed\t\talpha\tupdateGradSigma\tw\n")
+            print("{}\t\t{:.5f}\t\t{:.4f}\t\t{:.3E}\t\t{:.3f}\t\t{:.4f}\t\t{:.4f}\t\t{:.4E}\t{:.4f}".format(t, theta, sigma, J, gradK, gradSigma, gradMixed, alpha, updateGradSigma))
 
         # Update parameters
 
         theta += alpha * gradK
-        sigma += alphaSigma * updateGradSigma
+        sigma_fun.update(alphaSigma, updateGradSigma)
 
-        # Bound the sigma to be always positive
-        sigma = max(MIN_SIGMA, sigma)
 
     if filename is not None:
         np.save(filename, traj_data)
         np.save(filename[:-4] + '_params.npy', initial_configuration)
 
 
-if __name__ == '__main__':
-    # run_experiment(sigma=1, theta=-0.1, alphaSigma=0.01, experiment_type=Constants.LOCAL_STEP_LENGTH)
-    # exit()
+def maybe_make_dir(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-    params = {
-        'sigma' : [1, 2, 5, 10, 20, 30],
-        'theta' : [-0.1],
-        'alphaSigma' : [0.1, 0.01, 0.001, 0.0001],
-        #'experiment_type' : [Constants.ONLY_THETA, Constants.THETA_AND_SIGMA, Constants.THETA_AND_MIXED, Constants.FAR_SIGHTED_STEP, Constants.LOCAL_STEP_LENGTH]
-        'experiment_type' : [Constants.ONLY_DELTA]
-    }
-
-    base_folder = 'experiments_sigma'
+def run_multiple_experiments(params, base_folder, num_iterations=10000):
+    maybe_make_dir(base_folder)
 
     for param in itertools.product(*params.values()):
         param_dict = dict(zip(params.keys(), param))
-        print("Sigma: {sigma}, Theta: {theta}, alphaSigma: {alphaSigma}, experiment_type: {experiment_type}".format(**param_dict))
 
-        filename = os.path.join(base_folder, "exp_{sigma}_{theta}_{alphaSigma}_{experiment_type}.npy".format(**param_dict))
+        print("Sigma: {sigma_fun}, Theta: {theta}, alphaSigma: {alphaSigma}, experiment_type: {experiment_type}".format(**param_dict))
 
-        run_experiment(**param_dict, filename=filename, verbose=False, n_iterations=10000)
+        filename = os.path.join(base_folder, "exp_{sigma_fun}_{theta}_{alphaSigma}_{experiment_type}.npy".format(**param_dict))
+
+        run_experiment(**param_dict, filename=filename, verbose=False, n_iterations=num_iterations)
+
+if __name__ == '__main__':
+    # l = Identity(1)
+    # run_experiment(sigma_fun=l, theta=-0.1, alphaSigma=0.01, experiment_type=Constants.ONLY_DELTA, n_iterations=100000, verbose=False)
+    # run_experiment(sigma_fun=l, theta=-0.1, alphaSigma=0.01, experiment_type=Constants.ONLY_DELTA, n_iterations=100000, verbose=False)
+    # exit()
+
+    params = {
+        #'sigma_fun' : [Identity(x) for x in [1, 2, 3]] + [Exponential(x) for x in [0, 0.1]],
+        'sigma_fun' : [Exponential(0)],
+        'theta' : [-0.1],
+        'alphaSigma' : [0.005, 0.01, 0.001, 0.0005, 0.02],
+        'experiment_type' : [Constants.ONLY_THETA, Constants.THETA_AND_SIGMA, Constants.THETA_AND_MIXED, Constants.FAR_SIGHTED_STEP, Constants.LOCAL_STEP_LENGTH, Constants.ONLY_DELTA]
+        #'experiment_type' : [Constants.ONLY_THETA, Constants.THETA_AND_SIGMA, Constants.ONLY_DELTA]
+    }
+
+    base_folder = 'experiments_sigma2'
+
+    run_multiple_experiments(params, base_folder, 100000)
